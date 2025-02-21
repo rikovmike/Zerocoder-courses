3f35f5944006bdfda10ae6033037c2252258ad43
rikovmike <rikovmike@gmail.com>
2025-02-21 12:42:10 +0300

    VD04 added

---
+++ VD04/DZ01.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return f"<h1>Текущие дата и время: {current_time}</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
