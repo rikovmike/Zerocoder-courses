c929eab9611777c472fb410a8040a3d7604fd37b
rikovmike <rikovmike@gmail.com>
2025-03-05 11:51:07 +0300

    VD07 added

---
+++ VD07/app/routes.py
@@ -0,0 +1,72 @@
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, current_user, login_required
+from app.models import User
+from app import app, db, bcrypt
+from app.forms import RegistrationForm, LoginForm
+from .forms import UpdateProfileForm
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form, title='Register')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            return redirect(url_for('home'))
+        else:
+            flash('Введены неверные данные')
+    return render_template('login.html', form=form, title='Login')
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/account',methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateProfileForm()
+
+    if form.validate_on_submit():
+        # Обновляем только те поля, которые были изменены
+        if form.username.data != current_user.username:
+            current_user.username = form.username.data
+        if form.email.data != current_user.email:
+            current_user.email = form.email.data
+        if form.password.data:
+            current_user.password = form.password.data  # Пароль обновляется только если он был введен
+
+        db.session.commit()  # Сохраняем изменения в базе данных
+        flash('Your profile has been updated!', 'success')
+        return redirect(url_for('account'))
+
+    # Заполняем форму текущими данными пользователя
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    return render_template('account.html', form=form)
+
+
